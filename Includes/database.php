f6d30b963ec3db8956d281af87163896cf031bd7
Levant Dylan <99670864+Dylserker@users.noreply.github.com>
2024-12-20 09:09:54 +0100

    Delete Includes directory

--- Includes/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=coda_school_new','root');
-} catch (Exception $e) {
-    $errors[] = "Erreur de connexion à la bdd {$e->getMessage()}";
-}
-?>
